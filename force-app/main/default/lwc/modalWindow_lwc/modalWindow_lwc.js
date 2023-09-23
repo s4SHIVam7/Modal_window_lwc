09808993b7f732af1474ed22e9b94a563b14a647
s4SHIVam7 <shiv.laidwar@gmail.com>
2023-09-24 00:23:19 +0530

    updated changes

--- force-app/main/default/lwc/modalWindow_lwc/modalWindow_lwc.js
+++ force-app/main/default/lwc/modalWindow_lwc/modalWindow_lwc.js
@@ -1,4 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 export default class ModalWindow_lwc extends LightningElement {
+
+    @track isModalOpen = false;
+
+    openModal() {
+        this.isModalOpen = true;
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+    }
 
 }
